9222abbf5a5aff543528481bd4a2043c735384fd
yeom1989 <junghoon.yeom89@gmail.com>
2022-03-15 16:30:32 +0900

    ✨divide #4

--- src/main/java/com/okdevtv/tdd/calculator/Calculator.java
+++ src/main/java/com/okdevtv/tdd/calculator/Calculator.java
@@ -7,4 +7,22 @@ public class Calculator {
 			
 		return sum;
 	}
+	
+	public int subtract(int number1, int number2) {		
+		int sum = number1 - number2; 
+			
+		return sum;
+	}
+	
+	public int multiply(int number1, int number2) {		
+		int sum = number1 * number2; 
+			
+		return sum;
+	}
+	
+	public int divide(int number1, int number2) {		
+		int sum = number1 / number2; 
+			
+		return sum;
+	}
 }
--- src/test/java/com/okdevtv/tdd/calculator/CalculatorTest.java
+++ src/test/java/com/okdevtv/tdd/calculator/CalculatorTest.java
@@ -8,16 +8,33 @@ public class CalculatorTest {
 	
 	//test add
 	@Test
-	public void test() {
+	public void testAdd() {
 		Calculator calculator = new Calculator();
 		int result = calculator.add(1,2);
 		assertEquals(3, result);
 	}
 	//testSubtract
-	
+	@Test
+	public void testSubtract() {
+		Calculator calculator = new Calculator();
+		int result = calculator.subtract(1,2);
+		assertEquals(-1, result);
+	}
 	//testMultiply
+	@Test
+	public void testMultiply() {
+		Calculator calculator = new Calculator();
+		int result = calculator.multiply(1,2);
+		assertEquals(2, result);
+	}
 	
 	//testDivide
+	@Test
+	public void testDivide() {
+		Calculator calculator = new Calculator();
+		int result = calculator.divide(1,2);
+		assertEquals(0, result);
+	}
 	
 
 }
